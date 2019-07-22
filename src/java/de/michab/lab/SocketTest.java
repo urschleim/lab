a14804eb9ddf6e10d40caecfc1c094d580fcd41d
Michael Binz <michael.binz@daimler.com>
2019-07-22 10:29:37 +0200

    Added SocketTest application.

---
+++ src/java/de/michab/lab/SocketTest.java
@@ -0,0 +1,136 @@
+package de.michab.lab;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import org.jdesktop.application.CliApplication;
+
+/**
+ *
+ * @author micbinz
+ */
+public class SocketTest extends CliApplication
+{
+    private static final int testPort = 313;
+
+    private void doServeImpl( Socket socket ) throws Exception
+    {
+        InputStream is =
+                socket.getInputStream();
+        OutputStream os =
+                socket.getOutputStream();
+
+        while ( true )
+        {
+            int c = is.read();
+
+            if ( c == -1 )
+                break;
+            if ( c == 'x' )
+                break;
+
+            os.write( c );
+            os.write( '-' );
+        }
+
+        socket.close();
+
+        out( "Server thread shutdown.\n" );
+    }
+
+    private void doServe( Socket socket )
+    {
+        try {
+            doServeImpl( socket );
+        }
+        catch ( Exception e ) {
+            e.printStackTrace();
+        }
+    }
+
+    private void socketAcceptor( ServerSocket ss ) throws Exception
+    {
+        while ( true )
+        {
+            Socket socket =
+                    ss.accept();
+            out( "Connect ...\n" );
+            Thread t = new Thread(
+                    () -> doServe( socket ),
+                    getClass().getSimpleName() );
+            t.setDaemon(
+                    true );
+            t.start();
+        }
+    }
+
+    public void socketServer()
+    {
+        try ( ServerSocket serverSocket =
+                new ServerSocket( testPort ) )
+        {
+
+            socketAcceptor( serverSocket );
+        }
+        catch ( Exception e )
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private void startServer( boolean daemon )
+    {
+        Thread t = new Thread(
+                () -> socketServer(),
+                "start" );
+        t.setDaemon( daemon );
+        t.start();
+    }
+
+    @Command
+    public void startServer()
+    {
+        out( "Operation %s\n", currentCommand() );
+
+        startServer( false );
+    }
+
+    @Command
+    public void start() throws Exception
+    {
+        out( "Operation %s\n", currentCommand() );
+
+        startServer( true );
+
+        Socket socket = new Socket(
+                InetAddress.getLocalHost(),
+                testPort );
+
+        OutputStream os = socket.getOutputStream();
+        os.write( "micbinz\nx".getBytes() );
+        os.flush();
+        InputStream is = socket.getInputStream();
+
+        while ( true )
+        {
+            int c = is.read();
+            if ( c == -1 )
+                break;
+            System.out.write( c );
+        }
+
+        socket.close();
+    }
+
+    /**
+     *
+     * @param argv
+     */
+    public static void main( String[] argv )
+    {
+        launch( SocketTest.class, argv );
+    }
+}
