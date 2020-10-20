a3065d0444f7ad6df57b7b38576dee602a600a7d
Michael Binz <michael.binz@daimler.com>
2020-10-20 10:05:42 +0200

    Tetrahedron factored out.

--- src/main/java/moleculesampleapp/SphereAndBox.java
+++ src/main/java/moleculesampleapp/SphereAndBox.java
@@ -6,8 +6,6 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.shape.MeshView;
-import javafx.scene.shape.TriangleMesh;
 
 
 public class SphereAndBox extends Lab3dApplication
@@ -15,33 +13,33 @@ public class SphereAndBox extends Lab3dApplication
     @Override
     protected Parent createActors( List<Node> toolbar )
     {
-        float[] points = {
-                10,10,10,
-                20,20,0,
-                0,20,0,
-                10,20,20
-        };
-        // Reuse a single texture mapping.
-        float[] texCoo = {
-                0f,0f,
-                0f,1f,
-                1f,0f
-        };
-        int[] faces = {
-                0,0,2,1,1,2,
-                0,0,1,2,2,1,
-                0,0,1,1,3,2,
-                0,0,3,2,1,1,
-                0,0,3,1,2,2,
-                0,0,2,2,3,1,
-                1,0,3,1,2,2,
-                1,0,2,2,3,1
-        };
-        var mesh = new TriangleMesh();
-        mesh.getPoints().addAll( points );
-        mesh.getTexCoords().addAll( texCoo );
-        mesh.getFaces().addAll( faces );
-        var meshView = new MeshView( mesh );
+//        float[] points = {
+//                10,10,10,
+//                20,20,0,
+//                0,20,0,
+//                10,20,20
+//        };
+//        // Reuse a single texture mapping.
+//        float[] texCoo = {
+//                0f,0f,
+//                0f,1f,
+//                1f,0f
+//        };
+//        int[] faces = {
+//                0,0,2,1,1,2,
+//                0,0,1,2,2,1,
+//                0,0,1,1,3,2,
+//                0,0,3,2,1,1,
+//                0,0,3,1,2,2,
+//                0,0,2,2,3,1,
+//                1,0,3,1,2,2,
+//                1,0,2,2,3,1
+//        };
+//        var mesh = new TriangleMesh();
+//        mesh.getPoints().addAll( points );
+//        mesh.getTexCoords().addAll( texCoo );
+//        mesh.getFaces().addAll( faces );
+        var meshView = new Tetrahedron();
 
         var result = new Group( meshView );
         var scale = 4d;
