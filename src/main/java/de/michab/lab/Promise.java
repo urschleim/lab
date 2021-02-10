17ad91363fbdad9d1c6e2428eaa7416b7dede3ff
Michael Binz <michab66@gmail.com>
2021-02-10 22:52:06 +0100

    Added operations with timoeout.

--- src/main/java/de/michab/lab/Promise.java
+++ src/main/java/de/michab/lab/Promise.java
@@ -1,6 +1,8 @@
 package de.michab.lab;
 
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * A promise to deliver a value.  The common use is that a producer
@@ -39,13 +41,44 @@ public class Promise<T>
      */
     public synchronized T get() throws InterruptedException
     {
+        _rLock.acquire();
+
         try
         {
-            _rLock.acquire();
             return _value;
         }
         finally
         {
+            _value = null;
+            _wLock.release();
+        }
+    }
+
+    /**
+     * Get the value with a timeout.  If the value is not yet set
+     * (see {@link #set(Object)}), the operation blocks until the
+     * timeout is reached.
+     *
+     * @param timeout The maximum time to wait for a value.
+     * @param unit The time unit of the timeout argument.
+     * @return The value.
+     * @throws InterruptedException In case of an interrupt.
+     * @throws TimeoutException If no value has been set in the
+     * timeout period.
+     */
+    public synchronized T get( long timeout, TimeUnit unit )
+            throws InterruptedException, TimeoutException
+    {
+        if ( ! _rLock.tryAcquire( timeout, unit ) )
+            throw new TimeoutException();
+
+        try
+        {
+            return _value;
+        }
+        finally
+        {
+            _value = null;
             _wLock.release();
         }
     }
@@ -60,8 +93,35 @@ public class Promise<T>
      */
     public void set( T value ) throws InterruptedException
     {
+        _wLock.acquire();
+
         try {
-            _wLock.acquire();
+            _value = value;
+        }
+        finally
+        {
+            _rLock.release();
+        }
+    }
+
+    /**
+     * Set the value with a timeout.  This may be called repeatedly.  If the
+     * previously set value was not already read (see {@link #get()}) then
+     * this operation blocks until the timeout is reached.
+     *
+     * @param value The promised value.
+     * @param timeout The maximum time to wait.
+     * @param unit The time unit of the timeout argument.
+     * @throws InterruptedException In case of an interrupt.
+     * @throws TimeoutException If the previously set value was not read in
+     * time.
+     */
+    public void set( T value, long timeout, TimeUnit unit ) throws InterruptedException, TimeoutException
+    {
+        if ( ! _wLock.tryAcquire( timeout, unit ) )
+            throw new TimeoutException();
+
+        try {
             _value = value;
         }
         finally
