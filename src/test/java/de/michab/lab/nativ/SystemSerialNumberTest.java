719adf092ce154435062cd521416aec4ff1d42bd
Michael Binz <michab66@gmail.com>
2023-12-03 18:23:39 +0100

    Added test.

---
+++ src/test/java/de/michab/lab/nativ/SystemSerialNumberTest.java
@@ -0,0 +1,23 @@
+package de.michab.lab.nativ;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.jdesktop.util.PlatformType;
+import org.junit.jupiter.api.Test;
+import org.smack.util.StringUtil;
+
+public class SystemSerialNumberTest
+{
+
+    @Test
+    public void lumumbaTest() throws Exception
+    {
+        var ssn =
+                PlatformType.load( SystemSerialNumber.class );
+        assertNotNull( ssn );
+
+        var sn = ssn.getSerialNumber();
+        assertTrue( StringUtil.hasContent( sn ) );
+    }
+}
